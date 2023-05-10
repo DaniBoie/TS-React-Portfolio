93c1a6b1698efa51813c8b6329ce351733325e47
Daniel Ayala <dannyboy3385@gmail.com>
2023-05-10 13:27:59 -0700

    sticky header

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import {Greet} from "./components/Greet"
+import {About} from "./components/About"
 
 function App() {
   return (
     <div className="App">
       <Greet name={"Daniel Ayala"}/>
+      <About />
     </div>
   );
 }
