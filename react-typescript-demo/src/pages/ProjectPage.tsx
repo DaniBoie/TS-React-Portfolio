8610c84f098c65ddcaa196951dea2e290acd7bf4
Daniel Ayala <dannyboy3385@gmail.com>
2023-06-02 09:34:57 -0700

    project page outline

--- react-typescript-demo/src/pages/ProjectPage.tsx
+++ react-typescript-demo/src/pages/ProjectPage.tsx
@@ -15,21 +15,50 @@ export const ProjectPage = () => {
 
       <div className="projectBody">
 
+        <p className="projectProblemStatement">Problem Statement:</p>
+
         <div className="projectSection">
           <div></div>
           <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
-          <p>Secret Sauce began as a class exercise for Human Computer Interaction, where we were instructed to solve a novel problem in the world.  Our team first brainstormed problem statements individually, then came together on a final decision based on weighted analysis. </p>
+          <p>Secret Sauce began as a class exercise for Human Computer Interaction, where we were instructed to solve a novel problem in the world. Our team first brainstormed problem statements individually, then came together on a final decision based on weighted analysis.</p>
           <div></div>
         </div>
         
         <div className="projectSection">
           <div></div>
-          <p>After everyone on the team was onboard with the problem statement we conducted a competitive analysis to understand what applications are already on the market. We then had another brainstorming session to ideate the main features of the application that we believed would satisfy the problem statement while making our application stand out.</p>
+          <p>hardest problems to address was differentiating our product from the field... My contribution was ____. To do so, I ____...  Key finding: ______.</p>
           <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
           <div></div>
           
         </div>
 
+        <div className="projectSection">
+          <div></div>
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
+          <p>user research... highlight the parts of the research that reiterate the value in the "gap" you found</p>
+          <div></div>
+        </div>
+
+        <div className="projectDesign">
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
+        </div>
+      </div>
+
+      <div className="projectLessonsHeader">
+          <p className="projectLogo">Reflection / Lessons Learned</p>
+      </div>
+
+      <div className="projectSection">
+        <div></div>
+        <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/react-typescript-demo/src/resources/astro.jpg)'} />
+        <p>what I learned and what I would do if we moved forward?</p>
+        <div></div>
+      </div>
+      <div>
+        <video className="projectVideo" src=""></video>
       </div>
     </>
   )
